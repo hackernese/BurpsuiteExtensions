c3bdbe1419d0d34d58956ecc75479de49d1ed676
Hackernese <zenixcybercube2k2@gmail.com>
2025-02-09 08:29:24 +0700

    Whatever

---
+++ Stringified JSON/src/main/java/com/jsonstringify/app/Extension.java
@@ -0,0 +1,13 @@
+package com.jsonstringify.app;
+
+import burp.api.montoya.BurpExtension;
+import burp.api.montoya.MontoyaApi;
+import burp.api.montoya.http.handler.HttpHandler;
+
+public class Extension implements BurpExtension {
+
+    @Override
+    public void initialize(MontoyaApi api) {
+    }
+
+}
